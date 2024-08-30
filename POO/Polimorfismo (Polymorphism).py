50f8d3c02d61ae9d16f3362918e1d2f909bdb4e3
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-30 16:54:19 -0300

    Polimorfismo de classe

---
+++ POO/Polimorfismo (Polymorphism).py
@@ -0,0 +1,21 @@
+#Polimorfismo de classe é sub escrever algo herdado de outra classe
+class Animal:
+    def emitir_som(self):
+        print('Qualquer som...')
+
+
+class Cachorro(Animal):
+    def emitir_som(self):
+        print('Au au!')
+
+
+class Gato(Animal):
+    def emitir_som(self):
+        print('Miau!')
+
+
+cachorro = Cachorro()
+cachorro.emitir_som()
+
+gato = Gato()
+gato.emitir_som()
